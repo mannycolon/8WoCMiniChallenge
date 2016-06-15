3e8ef81343b10ec937ca2feb33e47bc25b9ebb7c
Luke Wilson <lukewilson@Lukes-MacBook-Air.local>
2016-06-15 17:01:18 -0400

    improved doccumentation and formatting, still have some ways to go

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+//Defining styles for our Opentips
 Opentip.styles.word = {
 	delay:0,
 	showOn: 'click',
@@ -32,26 +33,26 @@ $("#scripture").mouseup(function(ev) {
 	// return if no selection made
 	if (selection == "") return;
 	document.execCommand('copy');
-	console.log(selection);
 	// regex to test for multiple words
 	var reg = /.+ .+/g;
 	// multi word selection
 	if (reg.test(selection) == true) {
-
+		//we have yet to handle this!
 	}
 	else { // single word selection
 		if ($(ev.target).data('opentips') != undefined) return;
 		var word = selection.replace(/ /g, "");
 		var strong = $(ev.target).attr('strong');
 		var lex = lexicon[strong];
-		console.log("Strong : " + strong);
+		//If the definition is not in our lexicon
 		if (lex == undefined) {
-			console.log("Lexicon entry not found");
 			$(ev.target).opentip('definition unavailable', {style: "word"});
 			return;
 		}
+		//If the definition is in our lexicon
 		var shortDef = lex.brief;
 		var morph = lex.morphology;
+		//Formatting the text in our tooltip to be displayed
 		$(ev.target).opentip("<b>" + word + "</b>" + " - "
 			+ shortDef
 			+ "<br></br><i>(" + morph + ")</i>" , {
@@ -59,7 +60,7 @@ $("#scripture").mouseup(function(ev) {
 	}
 });
 
-
+//Goes back a chapter when clicked
 $("#previousChapter").click(function(){
 	if(chapterNumber >= 2){
 	chapterNumber--;
@@ -67,7 +68,7 @@ $("#previousChapter").click(function(){
 	}
 })
 
-
+//Goes foward a chapter when clicked
 $("#goToChapter").click(function(){
 	var desiredChapter = parseInt($("#chapterBox").val());
 	if(desiredChapter <= 6 && desiredChapter >= 1){
@@ -86,21 +87,35 @@ function getScripture(url){
 	type: 'get',
 	cache: false,
 	success: function(data){
+
+		//This regex finds our strong numbers
 		var reg = /[^A-Za-z0-9 -]+ G[0-9]{2,6}/g;
 		var result = [];
+
+		//Iteration goes through chapters then verses
 		$.each(data.Ephesians, function(chap, verses) {
 	      $.each(verses, function(verse, text) {
+
+	      	//We remove all the text in {} and remove the []
 	      	text = text.replace(/{.*}|[\[\]]/g, "");
+
+	      	//Placing the verse number in our span so that we can add it to the users clip board later
 	      	var newVerse = "<span verse='" + verse + "'>";
+
+	      	//Adds the strong number to a strong container on the span and adds places the word in the wrapper
 	      	while(result = reg.exec(text)){
 	      		var a = result[0].split(" ");
 	      		newVerse += "<span strong ='" + a[1].substring(1) + "'>" + a[0] + "</span> ";
 	      	}
+
+	      	//Closing our span tag for the verse
 	      	newVerse += "</span>"
 	        data.Ephesians[chap][verse] = newVerse;
 	      });
 	    });
 		book = data;
+
+		//Update the view with data we got from our AJAX call
 		displayScripture(data, chapterNumber);
 	},
 	error: function(err) {
@@ -115,6 +130,9 @@ $.ajax({
 	dataType: 'json',
 	type: 'get',
 	cache: false,
+
+	//Here we read in our lexicon and place it into a new structure
+	//TODO: EVAN could you maybe explain this better?
 	success: function(data) {
 		for (var i = 0; i < data.length; i++) {
 			lexicon[data[i].strongs] = data[i];
